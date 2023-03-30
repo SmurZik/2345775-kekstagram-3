4997e40084feee6b189b630da0ec08f97eea208b
smurzik <murca2403@gmail.com>
2023-03-30 21:58:33 +0300

    task 7.15

---
+++ js/picture.js
@@ -0,0 +1,15 @@
+import {getPhotos} from './data.js';
+
+const pictureFragment = document.createDocumentFragment();
+const pictureList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content;
+const photos = getPhotos(25);
+
+photos.forEach(({url, likes, comments})=>{
+  const picture = pictureTemplate.cloneNode(true);
+  picture.querySelector('.picture__img').src = url;
+  picture.querySelector('.picture__likes').textContent = likes;
+  picture.querySelector('.picture__comments').textContent = comments;
+  pictureFragment.appendChild(picture)
+});
+pictureList.appendChild(pictureFragment);
